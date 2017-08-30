ba18331955d4655def5afcc50dc039e8fd283edf
Sebastian <seppel9595@web.de>
2017-08-30 08:25:15 +0200

    Translations for InGameHelp Page
    Added Translations to InGameHelp Page

--- apps/frontend/modules/main/templates/ingameSuccess.php
+++ apps/frontend/modules/main/templates/ingameSuccess.php
@@ -1,61 +1,61 @@
-<h3><?php echo __("Aide InGame"); ?></h3>
+<h3><?php echo __("InGame Help"); ?></h3>
 
 <div class="well">
     <p>
-        <?php echo __("L'eBot possède tout une série de commande qui peuvent être appelée à différent moment dans le jeu via le tchat. Ces commandes permettent de demande par exemple ses stats, le score du match, démarrer le match, etc ..."); ?>
+        <?php echo __("eBot includes a whole series of commands which can be used via the chat, at various moments of the match. With these commands you can ask for your personal stats, the score match, you can restart the match, etc ..."); ?>
     </p>
 </div>
 <hr/>
 
-<h5><?php echo __("Les commandes utilisables à tout moment"); ?></h5>
+<h5><?php echo __("Commands usable at any moment"); ?></h5>
 
 <div class="well">
     <ul class="unstyled">
-        <li><b>!help</b> : <?php echo __("affiche les commandes disponibles"); ?></li>
-        <li><b>!stats</b> : <?php echo __("envoie les statistiques du matchs (nombre de kill, ratio K/D, ratio HS, ...)"); ?></li>
-        <li><b>!morestats</b> : <?php echo __("envoie les statistiques détaillées, tels que le nombre de First Kill, 1vX, kill par round, etc ..."); ?></li>
-        <li><b>!status</b> : <?php echo __("affiche le statut du match"); ?></li>
-        <li><b>!score</b> : <?php echo __("envoie les scores du matchs"); ?></li>
+        <li><b>!help</b> : <?php echo __("displays the available commands"); ?></li>
+        <li><b>!stats</b> : <?php echo __("sends the statistics of the match (number of kills, K/D ratio, HD ratio, ...)"); ?></li>
+        <li><b>!morestats</b> : <?php echo __("sends the detailed statistics, such as the number of First Kill, 1vX, kill per round, etc ..."); ?></li>
+        <li><b>!status</b> : <?php echo __("displays the match status"); ?></li>
+        <li><b>!score</b> : <?php echo __("sends the match scores"); ?></li>
     </ul>
 </div>
 
-<h5><?php echo __("Les commandes utilisables durant le warmup"); ?></h5>
+<h5><?php echo __("Commands usable during warmup"); ?></h5>
 
 <div class="well">
     <ul class="unstyled">
-        <li><b>!ready</b> : <?php echo __("signaler au bot que l'équipe est ready. Lorsque les 2 équipes sont ready, le match est lancé"); ?></li>
-        <li><b>!notready</b> : <?php echo __("signaler au bot que l'équipe n'est plus ready."); ?></li>
+        <li><b>!ready</b> : <?php echo __("telling the bot that the team is ready. When the 2 teams are ready, the match is launched"); ?></li>
+        <li><b>!notready</b> : <?php echo __("telling the bot that the team is not ready anymore."); ?></li>
     </ul>
 </div>
 
-<h5><?php echo __("Les commandes utilisables durant le knife round"); ?></h5>
+<h5><?php echo __("Commands usable during knife round"); ?></h5>
 
 <div class="well">
     <ul class="unstyled">
-        <li><b>!restart</b> : <?php echo __("signal que votre équipe veut un redémarrage du knife round. Le redémarrage du knife round n'est possible que si l'autre équipe est d'accord."); ?></li>
+        <li><b>!restart</b> : <?php echo __("tells that your team wants the knife round to be restarted. The restart of the knife round is only possible if the other team agrees."); ?></li>
     </ul>
 </div>
 
-<h5><?php echo __("Les commandes utilisables à la fin du knife round"); ?></h5>
+<h5><?php echo __("Commands usable at the end of the knife round"); ?></h5>
 
 <div class="well">
     <p>
-        <?php echo __("A la fin du knife round, l'équipe gagnante <b>DEVRA</b> signaler à l'eBot ce qu'il décide (stay ou switch). L'eBot switchera automatiquement les joueurs et inversera les équipes après."); ?>
+        <?php echo __("At the end of the knife round, the winning team &lt;b&gt;WILL HAVE TO&lt;/b&gt; tell the eBot what they decide to do (stay or switch). The eBot will automatically switch the players and reverse the team."); ?>
     </p>
     <ul class="unstyled">
-        <li><b>!stay</b> : <?php echo __("pas de changement"); ?></li>
-        <li><b>!switch</b> : <?php echo __("switch les équipes"); ?></li>
+        <li><b>!stay</b> : <?php echo __("no team switch"); ?></li>
+        <li><b>!switch</b> : <?php echo __("switches the teams"); ?></li>
     </ul>
 </div>
 
 
-<h5><?php echo __("Les commandes utilisables durant le match"); ?></h5>
+<h5><?php echo __("Commands usable during the match"); ?></h5>
 
 <div class="well">
     <ul class="unstyled">
-        <li><b>!stop</b> : <?php echo __("signal que votre équipe veut un arrêt du match. L'arrêt du match n'est possible que si l'autre équipe est d'accord. Elle devra écrire à son tour !stop pour arrêter le round en cours."); ?></li>
-        <li><b>!continue</b> : <?php echo __("signal que votre équipe est prête pour reprendre le match (dans le cas d'un stop)"); ?></li>
-        <li><b>!pause</b> : <?php echo __("signal que votre équipe veut une pause"); ?></li>
-        <li><b>!unpause</b> : <?php echo __("signal que votre équipe veut retirer la pause"); ?></li>
+        <li><b>!stop</b> : <?php echo __("tells the other team that your team wants to stop the match. The match stop is only possible if the other team agrees. The other team will also have to write !stop to stop the match. The match stop makes the match return to the previous state (warmup)."); ?></li>
+        <li><b>!continue</b> : <?php echo __("tells the other team your team wants to continue the match (if stopped)"); ?></li>
+        <li><b>!pause</b> : <?php echo __("tells the other team you want to pause"); ?></li>
+        <li><b>!unpause</b> : <?php echo __("tells the other team you want to unpause"); ?></li>
     </ul>
 </div>
